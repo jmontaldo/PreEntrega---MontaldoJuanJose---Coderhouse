4d07abc8d31f507ab8b168544046660653553318
Juan Montaldo <juanjosemontaldo@hotmail.com>
2024-05-15 10:21:21 -0300

    Se agregan forms para 'Vuelos' y se mejora el aspecto de algunas interacciones

--- Aerolinea/Vuelos/urls.py
+++ Aerolinea/Vuelos/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import vuelos
+from .views import vuelos, vuelos_create
 
 app_name = "vuelos"
 
 urlpatterns = [
     path("", vuelos, name='vuelos'),
+    path("Create/", vuelos_create, name='create'),
 ]
--- Aerolinea/Vuelos/views.py
+++ Aerolinea/Vuelos/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Vuelos.models import Vuelos
+from Vuelos.forms import VuelosForm
 
 def vuelos(request):
     consulta = Vuelos.objects.all()
     contexto = {"vuelos": consulta}
     return render(request, "Core/vuelos.html", contexto)
+
+def vuelos_create(request):
+    if request.method == "POST":
+        form = VuelosForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("vuelos:vuelos")
+    else:
+        form = VuelosForm()
+    return render(request, "Core/vuelos_create.html", {"form":form})
